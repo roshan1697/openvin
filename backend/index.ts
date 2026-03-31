11dc9b68cb547f59b79b98ca44845cf47083c065
roshan1697 <roshanpoojary16@gmail.com>
2026-03-31 23:40:06 +0530

    --

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import authRoutes from './routes/auth.routes'
+
+
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api/v1/auth", authRoutes)
+
+app.listen('3000',()=> console.log('server running'))
